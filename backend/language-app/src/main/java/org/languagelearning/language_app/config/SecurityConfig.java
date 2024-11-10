4fe5c509065f6d8cb67cb1ce44d3d1d4d340429d
irmaktekin <irmaktekin97@gmail.com>
2024-11-10 21:48:22 +0300

    Password encryption is implemented for users.

--- backend/language-app/src/main/java/org/languagelearning/language_app/config/SecurityConfig.java
+++ backend/language-app/src/main/java/org/languagelearning/language_app/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.languagelearning.language_app.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -9,8 +10,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -19,6 +25,8 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableMethodSecurity
 //Adding custom security config
 public class SecurityConfig {
+    @Autowired
+    DataSource dataSource;
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
@@ -30,17 +38,27 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService(){
         UserDetails user1 = User.withUsername("student")
-                .password("{noop}password1")
+                .password(passwordEncoder().encode("password1"))
                 .roles("STUDENT")
                 .build();
         UserDetails user2 = User.withUsername("teacher")
-                .password("{noop}passwordteacher")
+                .password(passwordEncoder().encode("passwordteacher"))
                 .roles("TEACHER")
                 .build();
         UserDetails user3 = User.withUsername("admin")
                 .password("{noop}adminpassword")
                 .roles("ADMIN")
                 .build();
-        return new InMemoryUserDetailsManager(user1,user2);
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.createUser(user1);
+        userDetailsManager.createUser(user2);
+        userDetailsManager.createUser(user3);
+        return userDetailsManager;
+
+
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
