b34107c69f19b5f4706b4f34880581eb43fdf692
irmaktekin <irmaktekin97@gmail.com>
2024-11-11 21:28:48 +0300

    UserDetailsService is implemented for finding the user.

---
+++ backend/language-app/src/main/java/org/languagelearning/language_app/service/MyUserDetailsService.java
@@ -0,0 +1,25 @@
+package org.languagelearning.language_app.service;
+
+import org.languagelearning.language_app.entities.User;
+import org.languagelearning.language_app.entities.UserPrincipal;
+import org.languagelearning.language_app.dao.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepo repo;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repo.findByUsername(username);
+        if(user==null){
+            System.out.println("User not found");
+            throw new UsernameNotFoundException("USER NOT FOUND");
+        }
+        return new UserPrincipal(user);
+    }
+}
